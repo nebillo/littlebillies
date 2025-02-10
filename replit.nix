6e234b0ad41d6a6b89c3592ac763a4709e781773
Nebil Kriedi <nkriedi@gmail.com>
2025-02-10 14:45:15 +0000

    activated venv

--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 { pkgs }: {
   deps = [
-    pkgs.python310  # Installa Python 3.10
+    pkgs.python310  # Usa Python 3.10
+    pkgs.python310Packages.pip  # Corretto: pip dentro python310Packages
   ];
 }
